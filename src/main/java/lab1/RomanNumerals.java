15f5eeb441209eb564f5260b0f681e11db8ea684
Louis Adjambri <louis.adjambri@lynx.italy>
2020-02-22 11:50:48 +0100

    [REFACTORING]: use arabicToRoman to dispatch unit and dozens

--- src/main/java/lab1/RomanNumerals.java
+++ src/main/java/lab1/RomanNumerals.java
@@ -12,16 +12,29 @@ public class RomanNumerals
     }
 
     public String arabicToRoman(int value) {
-          String result = "";
-          if(value == 5 )
-              return "V";
-          else if( value == 10)
-              return "X";
-          else if( value == 50)
-              return "L";
+        int  unit = value % 10;
+        int  dozens = (value % 100)  - unit;
+        return  getDozens(dozens) + getUnit(unit);
 
-          for(int i = 0; i < value ; i++)
-              result += "I";
-          return result;
     }
+
+    private String getDozens(int value) {
+        if( value == 10)
+            return "X";
+        else if( value == 50)
+            return "L";
+        return "";
+    }
+
+    private String getUnit(int value) {
+
+        String result ="";
+        if(value == 5 )
+            return "V";
+        for(int i = 0; i < value ; i++)
+            result += "I";
+        return result;
+    }
+
+
 }
